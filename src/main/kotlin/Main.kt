520e6f56fd2e62c7624fc5fe029012206a4ac2e9
Jerrobzz <jeremykipkirui254@gmail.com>
2023-04-19 15:24:50 +0300

    Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    var name = "Jeremy" //String
+    var age = 27 //Int
+    println(name)
+    println(age)
+
+    var a = 35
+    var b = 100
+    var sum = a + b
+    println(sum)
+
+}
