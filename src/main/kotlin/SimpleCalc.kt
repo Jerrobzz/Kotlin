9ab2393b1d68e33de6e72d576878253897991bec
Jerrobzz <jeremykipkirui254@gmail.com>
2023-04-19 16:44:28 +0300

    my commit

---
+++ src/main/kotlin/SimpleCalc.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Enter your first number")
+    var num1 = readLine()!!.toInt()
+    println("Enter your second number")
+    var num2 = readLine()!!.toInt()
+    var product = num1 * num2
+    println( product)
+
+
+
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println("---Enter Your Name---")
+    var name = readLine()!!.toString()
+    println(name)
+}
