ed2e0ef439b635caae4e8bd6c8b78b01dfe6e2ff
Jerrobzz <jeremykipkirui254@gmail.com>
2023-04-20 15:06:59 +0300

    Introduction to Kotlin Datatypes

---
+++ src/main/kotlin/Datatypes.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var city:String = "Nairobi"
+    var age:Int = 20
+    var domain:Char = 'K'
+    var status:Boolean = true
+    var temperature:Float = 54.4F
+    var results:Double = 45.8263
+}
